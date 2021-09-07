43a65a8b896714190ebb266ef008fb78560b04fc
Umar1523 <waseemexam1990@gmail.com>
2021-09-07 17:44:58 +0530

    Signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { Route } from 'react-router-dom';
 import React from 'react';
+import PropTypes from 'prop-types';
 import Homepage from './component/pages/Homepage';
 import Loginpage from './component/pages/Loginpage';
+import Dashboardpage from './component/pages/Dashboardpage';
+import Signuppage from './component/pages/Signuppage';
+import UserRoute from './component/routes/UserRoute';
+import GuestRoute from './component/routes/GuestRoute';
 
-const App=() =>
+
+const App=({ location }) => (
     <div className="ui container">
-     <Route path="/" exact component={Homepage} /> 
-      <Route path="/login" exact component={Loginpage} />
+     <Route location={location} path="/" exact component={Homepage} /> 
+      <GuestRoute location={location} path="/login" exact component={Loginpage} />
+      <GuestRoute location={location} path="/signup" exact component={Signuppage} />
+      <UserRoute location={location} path="/dashboard" exact component={Dashboardpage} />
     </div>
- 
+);
+
+App.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  }).isRequired
+};
+
 export default App;
