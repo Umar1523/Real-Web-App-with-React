64e613c7a57c9306919bdbd02642f32a93af2897
Umar1523 <waseemexam1990@gmail.com>
2021-09-07 21:18:00 +0530

    user email confirmation

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Homepage from './component/pages/Homepage';
 import Loginpage from './component/pages/Loginpage';
 import Dashboardpage from './component/pages/Dashboardpage';
 import Signuppage from './component/pages/Signuppage';
+import Confirmationpage from './component/pages/Confirmationpage';
 import UserRoute from './component/routes/UserRoute';
 import GuestRoute from './component/routes/GuestRoute';
 
 
 const App=({ location }) => (
     <div className="ui container">
-     <Route location={location} path="/" exact component={Homepage} /> 
+      <Route location={location} path="/" exact component={Homepage} /> 
+      <Route location={location} path="/confirmation/:token" exact component={Confirmationpage} /> 
       <GuestRoute location={location} path="/login" exact component={Loginpage} />
       <GuestRoute location={location} path="/signup" exact component={Signuppage} />
       <UserRoute location={location} path="/dashboard" exact component={Dashboardpage} />
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'semantic-ui-css/semantic.min.css';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import decode from 'jwt-decode';
 import { composeWithDevTools } from 'redux-devtools-extension'; 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -18,7 +19,12 @@ const store = createStore(
 );
 
 if(localStorage.bookwormJWT) {
-  const user = { token: localStorage.bookwormJWT };
+  const payload = decode(localStorage.bookwormJWT);
+  const user = { 
+    token: localStorage.bookwormJWT, 
+    email: payload.email, 
+    confirmed: payload.confirmed 
+  };
   store.dispatch(userLoggedIn(user));
 }
 
@@ -34,4 +40,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
